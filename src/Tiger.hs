09b5d86e734049125591e6b133773514d3a3048b
forestaa <t.daichi.3@gmail.com>
2017-12-20 09:05:37 +0900

    define Tiger and implement its interpreter

---
+++ src/Tiger.hs
@@ -0,0 +1,13 @@
+module Tiger where
+
+import qualified Data.Map as M
+
+type Id = String
+type Value = Int
+type Env = M.Map Id Value
+
+data Stm = CompoundStm Stm Stm
+         | AssignStm Id Exp
+         | PrintStm [Exp]
+data Exp = Id Id | Num Value | BiOp Exp Biop Exp | Eseq Stm Exp
+data Biop = Plus | Minus | Times | Div
