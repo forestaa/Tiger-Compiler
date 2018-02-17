a75d6893722d61278ead1ef2d73bed439aad6551
forestaa <t.daichi.3@gmail.com>
2018-02-17 17:59:08 +0900

    add test_all function to test tiger compiler through testcases

--- test/Tiger/Test.hs
+++ test/Tiger/Test.hs
@@ -1,27 +1,39 @@
 module Tiger.Test where
 
 import qualified Data.ByteString.Lazy as B
+-- import Contol.Monad
+import Control.Monad.Except
 
 import Lexer.Monad
 
+import Tiger.Syntax
 import Tiger.Lexer
 import Tiger.Parser
 
 
 test :: IO ()
-test = do
-  let file = "test/Tiger/samples/test02.tig"
-  scanner_test file
-  parser_test file
+test = test_all
 
-scanner_test :: FilePath -> IO ()
+test_all :: IO ()
+test_all = runExceptT (sequence $ fmap test_1 testcases) >>= print
+
+test_1 :: FilePath -> ExceptT String IO LExp
+test_1 file = do
+  ExceptT $ scanner_test file
+  ExceptT $ parser_test file
+
+scanner_test :: FilePath -> IO (Either String [Lexeme])
 scanner_test file = do
   bs <- B.readFile file
-  print $ scanner file bs
+  return $ scanner file bs
 
-parser_test ::FilePath -> IO ()
+parser_test ::FilePath -> IO (Either String LExp)
 parser_test file = do
   bs <- B.readFile file
-  print $ runP file bs parser
+  return $ runP file bs parser
 
+testcases :: [FilePath]
+testcases = (++) <$> (("test/Tiger/samples/test" ++) <$> cases) <*> [".tig"]
+  where
+    cases = ((:) '0' . show <$> [1..9]) ++ (show <$> [10..49])
 
