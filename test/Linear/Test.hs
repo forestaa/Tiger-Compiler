2c49ad7406015e7416276b1f14fd8a817dda4588
forestaa <t.daichi.3@gmail.com>
2018-02-19 16:01:49 +0900

    update: lts-10.5

--- test/Linear/Test.hs
+++ test/Linear/Test.hs
@@ -1,32 +1,31 @@
 module Linear.Test where
 
 import qualified Data.ByteString.Lazy as B
-
 import qualified Data.Map.Strict as M
+import Control.Monad
 
 import Lexer.Monad
 
 import Linear.Lexer
 import Linear.Parser
 import Linear.Interpreter
-import Linear.Syntax
 
-lexer_test :: IO ()
-lexer_test = do
+lexerTest :: IO ()
+lexerTest = do
   let file = "test/Linear/samples/test.ln"
   bs <- B.readFile file
   print $ scanner file bs
 
-parser_test :: IO ()
-parser_test = do
+parserTest :: IO ()
+parserTest = do
   let file = "test/Linear/samples/test.ln"
   bs <- B.readFile file
   print $ runP parser file bs
 
-interpreter_test :: IO ()
-interpreter_test = do
+interpreterTest :: IO ()
+interpreterTest = do
   let file = "test/Linear/samples/test.ln"
   bs <- B.readFile file
   case runP parser file bs of
-    Right ast -> run M.empty ast >> return ()
+    Right ast -> void $ run M.empty ast
     Left msg -> print msg
--- test/Tiger/Test.hs
+++ test/Tiger/Test.hs
@@ -1,7 +1,6 @@
 module Tiger.Test where
 
 import qualified Data.ByteString.Lazy as B
--- import Contol.Monad
 import Control.Monad.Except
 
 import Lexer.Monad
@@ -12,24 +11,24 @@ import Tiger.Parser
 
 
 test :: IO ()
-test = test_all
--- test = runExceptT (test_1 "test/Tiger/samples/merge.tig") >>= print
+test = testAll
+-- test = runExceptT (test1 "test/Tiger/samples/merge.tig") >>= print
 
-test_all :: IO ()
-test_all = runExceptT (sequence $ fmap test_1 testcases) >>= print
+testAll :: IO ()
+testAll = runExceptT (traverse test1 testcases) >>= print
 
-test_1 :: FilePath -> ExceptT String IO LExp
-test_1 file = do
-  ExceptT $ scanner_test file
-  ExceptT $ parser_test file
+test1 :: FilePath -> ExceptT String IO LExp
+test1 file = do
+  ExceptT $ scannerTest file
+  ExceptT $ parserTest file
 
-scanner_test :: FilePath -> IO (Either String [Lexeme])
-scanner_test file = do
+scannerTest :: FilePath -> IO (Either String [Lexeme])
+scannerTest file = do
   bs <- B.readFile file
   return $ scanner file bs
 
-parser_test :: FilePath -> IO (Either String LExp)
-parser_test file = do
+parserTest :: FilePath -> IO (Either String LExp)
+parserTest file = do
   bs <- B.readFile file
   return $ runP parser file bs
 
