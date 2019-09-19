ecfa6c05149a683ed7c02936f952fd6ddd506c15
forestaa <t.daichi.3@gmail.com>
2019-09-19 22:49:45 +0900

    define IR

--- src/IR.hs
+++ src/IR.hs
@@ -1,3 +1,51 @@
 module IR where
 
-data Stm = Nil
+import RIO
+
+import qualified Unique as U
+
+
+data Exp = Const Int
+         | Name U.Label
+         | Temp U.Temp
+         | BinOp BinOp Exp Exp
+         | Mem Exp
+         | Call Exp
+         | ESeq Stm Exp
+  deriving (Eq, Show)
+
+data Stm = Move Exp Exp
+         | Exp Exp
+         | Jump [(Exp, U.Label)]
+         | CJump RelOp Exp Exp U.Label U.Label
+         | Seq Stm Stm
+         | Label U.Label
+  deriving (Eq, Show)
+
+data BinOp = Plus
+           | Minus
+           | Mul
+           | Div
+           | And
+           | Or
+           | LShift
+           | RShift
+           | ARShift
+           | XOr
+  deriving (Eq, Show)
+
+data RelOp = Eq 
+           | Ne 
+           | Lt 
+           | Gt 
+           | Le 
+           | Ge 
+           | ULt 
+           | ULe 
+           | UGt 
+           | UGe
+  deriving (Eq, Show)
+
+seqStm :: [Stm] -> Stm
+seqStm [s] = s
+seqStm (s:ss) = Seq s $ IR.seqStm ss
