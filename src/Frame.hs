eb38ab7a2cc0839bde81288e6ce85e5fe16a544b
forestaa <t.daichi.3@gmail.com>
2019-09-22 21:14:54 +0900

    make access mono and add wordsize

--- src/Frame.hs
+++ src/Frame.hs
@@ -1,6 +1,6 @@
 module Frame where
 
-import RIO (Bool, String)
+import RIO (Int, Bool, String)
 
 import           Data.Proxy
 import qualified Unique as U
@@ -8,13 +8,14 @@ import qualified IR
 import Data.Extensible
 
 class Frame f where
-  type Access f
+  type Access f = a | a -> f
   newFrame :: Lookup xs "temp" U.UniqueEff => U.Label -> [Bool] -> Eff xs f
   name :: f -> U.Label
   formals :: f -> [Access f]
   allocLocal ::(Lookup xs "temp" U.UniqueEff) => f -> Bool -> Eff xs (f, Access f)
   fp :: Proxy f -> U.Temp
-  exp :: Proxy f -> Access f -> IR.Exp -> IR.Exp
+  exp :: Access f -> IR.Exp -> IR.Exp
+  wordSize :: Proxy f -> Int
 
 
 data Frag f where
