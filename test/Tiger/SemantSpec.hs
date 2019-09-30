058898be7ce7bb0e5316d843cf5522a941098ba3
forestaa <t.daichi.3@gmail.com>
2019-10-01 00:02:18 +0900

    add test for translateArrayIndex

--- test/Tiger/SemantSpec.hs
+++ test/Tiger/SemantSpec.hs
@@ -26,6 +26,7 @@ spec = do
   translateNilSpec
   translateVariableSpec
   translateRecordFieldSpec
+  translateArrayIndexSpec
 
 translateIntSpec :: Spec
 translateIntSpec = describe "translate int test" $ do
@@ -203,3 +204,73 @@ translateRecordFieldSpec = describe "translate record field test" $ do
         where
           isMissingRecordField MissingRecordField{} = True
           isMissingRecordField _ = False
+
+
+translateArrayIndexSpec :: Spec
+translateArrayIndexSpec = describe "translate array index test" $ do
+  it "array index x[0]" $ do
+    let ast = T.expToLExp $ T.Var (T.ArrayIndex (T.Id "x") (T.Int 0))
+        result = leaveEff . runTranslateEffWithNewLevel $ do
+          id <- getUniqueEff #id
+          let arrayTy = TArray $ #range @= TInt <: #id @= id <: nil
+          _ <- allocateLocalVariable "x" True arrayTy
+          translateExp @FrameMock ast
+    case result of
+      Left e -> expectationFailure $ show e
+      Right ((exp, ty), _) -> do
+        exp `shouldBe` Ex (IR.Mem (IR.BinOp IR.Plus (IR.Mem (IR.BinOp IR.Plus (IR.Const (-F.wordSize @FrameMock)) (IR.Temp (F.fp @FrameMock)))) (IR.BinOp IR.Mul (IR.Const 0) (IR.Const (F.wordSize @FrameMock)))))
+        ty `shouldBe` TInt
+
+  it "array index x[1 + 1]" $ do
+    let ast = T.expToLExp $ T.Var (T.ArrayIndex (T.Id "x") (T.Op (T.Int 1) T.Plus (T.Int 1)))
+        result = leaveEff . runTranslateEffWithNewLevel $ do
+          id <- getUniqueEff #id
+          let arrayTy = TArray $ #range @= TInt <: #id @= id <: nil
+          _ <- allocateLocalVariable "x" True arrayTy
+          translateExp @FrameMock ast
+    case result of
+      Left e -> expectationFailure $ show e
+      Right ((exp, ty), _) -> do
+        exp `shouldBe` Ex (IR.Mem (IR.BinOp IR.Plus (IR.Mem (IR.BinOp IR.Plus (IR.Const (-F.wordSize @FrameMock)) (IR.Temp (F.fp @FrameMock)))) (IR.BinOp IR.Mul (IR.BinOp IR.Plus (IR.Const 1) (IR.Const 1)) (IR.Const (F.wordSize @FrameMock)))))
+        ty `shouldBe` TInt
+
+  it "array index x.y[0]" $ do
+    let ast = T.expToLExp $ T.Var (T.ArrayIndex (T.RecField (T.Id "x") "y") (T.Int 0))
+        result = leaveEff . runTranslateEffWithNewLevel $ do
+          id1 <- getUniqueEff #id
+          id2 <- getUniqueEff #id
+          let arrayTy = TArray  $ #range @= TString <: #id @= id1 <: nil
+              recordTy = TRecord $ #map @= Map.fromList [("y", arrayTy)] <: #id @= id2 <: nil
+          _ <- allocateLocalVariable "x" True recordTy
+          translateExp @FrameMock ast
+    case result of
+      Left e -> expectationFailure $ show e
+      Right ((exp, ty), _) -> do
+        exp `shouldBe` Ex (IR.Mem (IR.BinOp IR.Plus (IR.Mem (IR.BinOp IR.Plus (IR.Mem (IR.BinOp IR.Plus (IR.Const (-F.wordSize @FrameMock)) (IR.Temp (F.fp @FrameMock)))) (IR.Const 0))) (IR.BinOp IR.Mul (IR.Const 0) (IR.Const (F.wordSize @FrameMock)))))
+        ty `shouldBe` TString
+
+  it "array index x['hoge']" $ do
+    let ast = T.expToLExp $ T.Var (T.ArrayIndex (T.Id "x") (T.String "hoge"))
+        result = leaveEff . runTranslateEffWithNewLevel $ do
+          id <- getUniqueEff #id
+          let arrayTy = TArray  $ #range @= TInt <: #id @= id <: nil
+          _ <- allocateLocalVariable "x" True arrayTy
+          translateExp @FrameMock ast
+    case result of
+      Right ret -> expectationFailure $ "should return ExpectedIntType" ++ show ret
+      Left e -> e `shouldSatisfy` isExpectedIntType
+        where
+          isExpectedIntType (L _ ExpectedIntType{}) = True
+          isExpectedIntType _ = False
+
+  it "array type expected" $ do
+    let ast = T.expToLExp $ T.Var (T.ArrayIndex (T.Id "x") (T.Int 0))
+        result = leaveEff . runTranslateEffWithNewLevel $ do
+          _ <- allocateLocalVariable "x" True TInt
+          translateExp @FrameMock ast
+    case result of
+      Right ret -> expectationFailure $ "should return ExpectedArrayType" ++ show ret
+      Left e -> e `shouldSatisfy` isExpectedIntType
+        where
+          isExpectedIntType (L _ ExpectedArrayType{}) = True
+          isExpectedIntType _ = False
