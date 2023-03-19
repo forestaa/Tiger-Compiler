8166e9c90aada2b66183e078c03abc083cc06969
forestaa <t.daichi.3@gmail.com>
2023-03-20 08:52:31 +0900

    feat: constrain interfered moves

--- src/Compiler/Backend/X86/RegisterAllocation/Coalesce/InterferenceGraph.hs
+++ src/Compiler/Backend/X86/RegisterAllocation/Coalesce/InterferenceGraph.hs
@@ -1,12 +1,13 @@
 module Compiler.Backend.X86.RegisterAllocation.Coalesce.InterferenceGraph
-  ( buildInterfereceGraph,
+  ( newInterferenceGraph,
+    buildInterfereceGraph,
   )
 where
 
 import Compiler.Backend.X86.Liveness qualified as L (ControlFlow (..), ControlFlowGraph, ControlFlowNode (..), newControlFlowGraph, solveDataFlowEquation)
-import Compiler.Backend.X86.RegisterAllocation.Coalesce.InterferenceGraph.Base (InterferenceGraphEdgeLabel (..), Move (destination, source), addMove, newMove)
+import Compiler.Backend.X86.RegisterAllocation.Coalesce.InterferenceGraph.Base qualified as B (InterferenceGraphEdgeLabel (..), addMove, constrainMove, newMove)
 import Compiler.Backend.X86.RegisterAllocation.Coalesce.InterferenceGraph.Immutable (InterferenceGraph)
-import Compiler.Backend.X86.RegisterAllocation.Coalesce.InterferenceGraph.Mutable qualified as Mutable (InterferenceMutableGraph, addEdge, addNode, empty, freeze, getEdges, getNode, updateNode)
+import Compiler.Backend.X86.RegisterAllocation.Coalesce.InterferenceGraph.Mutable qualified as Mutable (InterferenceMutableGraph, addEdge, addNode, empty, freeze, getEdges, getEdgesByIndex, getNode, updateNode)
 import Compiler.Utils.Graph.Base (Node (..))
 import Compiler.Utils.Graph.Immutable qualified as Immutable (ImmutableGraph (..))
 import GHC.Records (HasField (getField))
@@ -19,28 +20,41 @@ newInterferenceGraph vars cfGraph =
   runST $ do
     graph <- Mutable.empty
     forM_ vars $ Mutable.addNode graph
-    forM_ (getField @"val" <$> Immutable.getAllNodes cfGraph) $ addInterferenceGraphEdges graph
+    let allNodes = getField @"val" <$> Immutable.getAllNodes cfGraph
+    forM_ allNodes $ addInterferenceGraphEdges graph
+    forM_ allNodes $ addMove graph
     Mutable.freeze graph
   where
     addInterferenceGraphEdges :: (PrimMonad m, Ord var, MonadThrow m) => Mutable.InterferenceMutableGraph var (PrimState m) -> L.ControlFlowNode var val -> m ()
     addInterferenceGraphEdges graph cnode
-      | cnode.isMove =
-          let notUsedOutput = cnode.liveOutVariables Set.\\ cnode.usedVariables Set.\\ cnode.definedVariables
-              moves = newMove <$> Set.toList cnode.usedVariables <*> Set.toList cnode.definedVariables
-           in do
-                forM_ moves $ \move -> do
-                  source <- Mutable.getNode graph move.source
-                  Mutable.updateNode graph source.index $ addMove move source.val
-                  target <- Mutable.getNode graph move.destination
-                  Mutable.updateNode graph target.index $ addMove move target.val
-                sequence_ $ addEdge <$> Set.toList cnode.definedVariables <*> Set.toList notUsedOutput
-      | otherwise =
-          let notDefinedOutput = cnode.liveOutVariables Set.\\ cnode.definedVariables
-           in sequence_ $ addEdge <$> Set.toList cnode.definedVariables <*> Set.toList notDefinedOutput
+      | cnode.isMove = do
+          let notUsedOutputs = cnode.liveOutVariables Set.\\ cnode.usedVariables Set.\\ cnode.definedVariables
+          forM_ cnode.definedVariables $ \definedVariable ->
+            forM_ notUsedOutputs $ \notUsedOutput ->
+              addEdge definedVariable notUsedOutput
+      | otherwise = do
+          let notDefinedOutputs = cnode.liveOutVariables Set.\\ cnode.definedVariables
+          forM_ cnode.definedVariables $ \definedVariable ->
+            forM_ notDefinedOutputs $ \notDefinedOutput ->
+              addEdge definedVariable notDefinedOutput
       where
         addEdge src tgt =
           whenM (Vec.null <$> Mutable.getEdges graph src tgt) . void $
-            Mutable.addEdge graph src tgt InterferenceGraphEdgeLabel
+            Mutable.addEdge graph src tgt B.InterferenceGraphEdgeLabel
+    addMove :: (PrimMonad m, Ord var, MonadThrow m) => Mutable.InterferenceMutableGraph var (PrimState m) -> L.ControlFlowNode var val -> m ()
+    addMove graph cnode
+      | cnode.isMove = do
+          srcs <- mapM (\src -> (src,) <$> Mutable.getNode graph src) $ Set.toList cnode.usedVariables
+          dests <- mapM (\dest -> (dest,) <$> Mutable.getNode graph dest) $ Set.toList cnode.definedVariables
+          forM_ srcs $ \(src, srcNode) ->
+            forM_ dests $ \(dest, destNode) -> do
+              edges <- Mutable.getEdgesByIndex graph srcNode.index destNode.index
+              let move = B.newMove src dest
+                  newSrcNode = if null edges then B.addMove move srcNode.val else B.constrainMove move srcNode.val
+                  newDestNode = if null edges then B.addMove move destNode.val else B.constrainMove move destNode.val
+              Mutable.updateNode graph srcNode.index newSrcNode
+              Mutable.updateNode graph destNode.index newDestNode
+      | otherwise = pure ()
 
 buildInterfereceGraph :: (Ord var) => Set.Set var -> [L.ControlFlow var val] -> InterferenceGraph var
 buildInterfereceGraph vars = newInterferenceGraph vars . L.solveDataFlowEquation . L.newControlFlowGraph
